7145ebdbdc151f65247ac616a12cf752f93f562e
cakmakov <anil.cakmak.tr@hotmail.com>
2017-07-03 20:57:59 +0300

    Parsing string datas from wired.com

---
+++ Wired/app/src/main/java/com/gycell/wired/Makale1.java
@@ -0,0 +1,78 @@
+package com.gycell.wired;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Makale1 extends AppCompatActivity {
+
+    private static String URL = "https://www.wired.com/2017/05/internet-week-120/";
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makale1);
+        ScrollView scrollView = (ScrollView)findViewById(R.id.scroll);
+
+        Intent intent = getIntent();
+
+        new VeriGetir().execute();
+    }
+
+    private class VeriGetir extends AsyncTask {
+
+        String desc;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(Makale1.this);
+            progressDialog.setTitle("AÇIKLAMA");
+            progressDialog.setMessage("Lütfen Bekleyiniz...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+
+            try {
+                Document doc = Jsoup.connect(URL).timeout(30*1000).get();
+
+                Elements elements = doc.select("article[data-js=content]");
+                desc = elements.text();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+
+
+            TextView textView = (TextView)findViewById(R.id.makale1);
+            textView.setText("Açıklama: "+""+desc);
+            progressDialog.dismiss();
+        }
+    }
+}
+
